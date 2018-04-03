50305a0c9ef25f7a9ea51030cd06802fa21d0e35
Leila Hadjioual <leila.hadjioual@Digital-Grenoble.local>
2018-04-03 15:43:43 +0200

    add class warrior

--- src/main/java/character/Warrior.java
+++ src/main/java/character/Warrior.java
@@ -1,4 +1,40 @@
 package main.java.character;
 
 public class Warrior {
+    private String weapon;
+    private String weaponLevel;
+    private String shield;
+    private int shieldLevel;
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public String getWeaponLevel() {
+        return weaponLevel;
+    }
+
+    public void setWeaponLevel(String weaponLevel) {
+        this.weaponLevel = weaponLevel;
+    }
+
+    public String getShield() {
+        return shield;
+    }
+
+    public void setShield(String shield) {
+        this.shield = shield;
+    }
+
+    public int getShieldLevel() {
+        return shieldLevel;
+    }
+
+    public void setShieldLevel(int shieldLevel) {
+        this.shieldLevel = shieldLevel;
+    }
 }
