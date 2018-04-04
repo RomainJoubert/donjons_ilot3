e45dd64c4ab99d0984e166faa6f8fe6948039882
Leila Hadjioual <leila.hadjioual@Digital-Grenoble.local>
2018-04-04 10:38:21 +0200

    lancement dé

--- src/main/java/game/Board.java
+++ src/main/java/game/Board.java
@@ -1,4 +1,13 @@
 package main.java.game;
+import java.util.Random;
 
 public class Board {
+
+    public static int nombreAleatoire(){
+            Random r = new Random();
+            int n = r.nextInt(6);
+            return n;
+
+    }
+
 }
--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,17 +1,44 @@
 package main.java.game;
+
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     private static Scanner sc = new Scanner(System.in);
 
-
     public static void main(String[] args) {
-        System.out.println("Choisissez un personnage \n 1-Guerrier \n 2-Magicien");
-        int chooseCharacter = sc.nextInt();
-        sc.nextLine();
-        System.out.println("Entrez le nom de votre personnage");
-        String chooseName = sc.nextLine();
-        System.out.println("vous avez le personnage : " +chooseCharacter+ " qui porte le nom de " + chooseName);
+        showMenu();
+        createcharacter();
+        nombreAleatoire();
 
+    }
+
+
+    public static void showMenu() {
+        System.out.println("Choisis un personnage \n 1-Guerrier \n 2-Magicien");
+        sc.nextInt();
+        sc.nextLine();
+    }
+
+    public static void createcharacter() {
+        System.out.println("Entre le nom de ton personnage");
+        String nameChoice = sc.nextLine();
+        System.out.println("Ajoute une image");
+        String pictureChoice = sc.nextLine();
+        System.out.println("Ton personnage porte le nom de " + nameChoice + " et a l'image : " + pictureChoice);
+    }
+
+    public static int nombreAleatoire() {
+        System.out.println("lancer le dé ? \n1- oui \n2-non");
+        int nb = sc.nextInt();
+        sc.nextLine();
+        int n = 0;
+        if (nb == 1) {
+            Random r = new Random(); // classe pour pouvoir utiliser le tirage aléatoire
+            n = r.nextInt(6) + 1; //permet un tirage entre 0 et 5, on y ajoute un pour ne pas tomber sur 0 et avoir 6
+            System.out.println(n);
+        } else {
+            System.out.println("hasta luego");
+        }return n;
     }
 }
